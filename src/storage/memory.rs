208ab4bfb23bd3accaaefd917b03c2cf19cfba73
Wei Liu <wei@hireguide.com>
2022-11-17 11:43:33 -0500

    add a get_or_create table method to Memtable

--- src/storage/memory.rs
+++ src/storage/memory.rs
@@ -1,14 +1,19 @@
 use dashmap::DashMap;
+use dashmap::mapref::one::Ref;
 
 use crate::Value;
 
 #[derive(Debug, Default, Clone)]
 pub struct MemTable {
-    data: DashMap<String, DashMap<String, Value>>,
+    tables: DashMap<String, DashMap<String, Value>>,
 }
 
 impl MemTable {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    fn get_or_create_table(&self, table_name: &str) -> Ref<String, DashMap<String, Value>> {
+        self.tables.entry(table_name.to_string()).or_insert_with(DashMap::new).downgrade()
     }
 }
