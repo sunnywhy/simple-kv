69ff944cc4bba91ba3e9f75a7e0aa6575b33df16
Wei Liu <wei@hireguide.com>
2022-11-17 11:18:16 -0500

    format

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,5 +1,5 @@
-use crate::pb::abi::{CommandRequest, Hset, KvPair, Value, value};
-use crate::pb::abi::command_request::RequestData;
+use abi::command_request::RequestData;
+use abi::*;
 
 pub mod abi;
 
@@ -16,14 +16,17 @@ impl CommandRequest {
 
 impl KvPair {
     pub fn new(key: impl Into<String>, value: Value) -> Self {
-        Self { key: key.into(), value: Some(value) }
+        Self {
+            key: key.into(),
+            value: Some(value),
+        }
     }
 }
 
 impl From<String> for Value {
     fn from(s: String) -> Self {
         Self {
-            value: Some(value::Value::String(s))
+            value: Some(value::Value::String(s)),
         }
     }
 }
@@ -31,7 +34,7 @@ impl From<String> for Value {
 impl From<&str> for Value {
     fn from(s: &str) -> Self {
         Self {
-            value: Some(value::Value::String(s.into()))
+            value: Some(value::Value::String(s.into())),
         }
     }
 }
